83aa4a01508d718ee8ca9373bd0fe26b3a0c5fda
b0dah <ries8898@gmail.com>
2021-09-10 20:43:19 +0300

    initial commit

---
+++ UdemyTwitter/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  UdemyTwitter
+//
+//  Created by Иван Романов on 10.09.2021.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+}
